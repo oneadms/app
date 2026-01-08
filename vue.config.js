37beb346923d235de6720a56abe8f632d014c001
chenhuahong <ojbk24680@gmail.com>
2026-01-08 08:12:42 +0800

    app

--- vue.config.js
+++ vue.config.js
@@ -9,14 +9,41 @@
 // +----------------------------------------------------------------------
 
 module.exports = {
-	productionSourceMap: true, // 生产打包时不输出map文件，增加打包速度,
+	productionSourceMap: false, // 关闭 source map 以减少内存占用
+	// 开发环境也关闭 source map
+	css: {
+		sourceMap: false
+	},
 	configureWebpack: config => {
+		// 开发环境也关闭 source map
+		config.devtool = false
+
 		if (process.env.NODE_ENV === 'production') {
-			//注释可看见打印，解开看不见
-			config.optimization.minimizer[0].options.terserOptions.compress.warnings = false
-			config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
-			config.optimization.minimizer[0].options.terserOptions.compress.drop_debugger = true
-			config.optimization.minimizer[0].options.terserOptions.compress.pure_funcs = ['console.log']
+			// 检查 minimizer 是否存在
+			if (config.optimization && config.optimization.minimizer && config.optimization.minimizer[0]) {
+				//注释可看见打印，解开看不见
+				if (config.optimization.minimizer[0].options && config.optimization.minimizer[0].options.terserOptions) {
+					config.optimization.minimizer[0].options.terserOptions.compress = config.optimization.minimizer[0].options.terserOptions.compress || {}
+					config.optimization.minimizer[0].options.terserOptions.compress.warnings = false
+					config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
+					config.optimization.minimizer[0].options.terserOptions.compress.drop_debugger = true
+					config.optimization.minimizer[0].options.terserOptions.compress.pure_funcs = ['console.log']
+				}
+			}
 		}
+	},
+	chainWebpack: config => {
+		// 优化构建性能
+		config.optimization.splitChunks({
+			chunks: 'all',
+			cacheGroups: {
+				libs: {
+					name: 'chunk-libs',
+					test: /[\\/]node_modules[\\/]/,
+					priority: 10,
+					chunks: 'initial'
+				}
+			}
+		})
 	}
 }
